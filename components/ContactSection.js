166d283a6955233c4629cee640b1827f326937fa
Rasmus Andersen <rasmus@growupstudio.dk>
2021-10-01 16:46:49 +0200

    third commit

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -1,5 +1,7 @@
 import css from '../styles/contact.module.scss'
 import Link from 'next/link'
+import Image from 'next/image'
+import arrow from '../public/arrow-up-right.svg'
 import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
@@ -30,13 +32,13 @@ export default function ContactSection() {
       <section className={css.contactSectionWrapper}>
 
       <h2 className={css.titleWrapper}>
-        <div className={`${css.contactTitle} contactWrapper`}><span className='contactInner'>Kontakt os</span></div>
+        <div className={`${css.contactTitle} contactWrapper`}><span className='contactInner'>Kontakt</span></div>
       </h2>
 
       <div className={css.contactInfoWrapper}>
         <div className={`${css.mailWrapper} contactWrapper`}>
           <span className='contactInner'>
-          <Link href='mailto:hello@growupstudio.dk'><a className={css.link}>Kontakt os her</a></Link>
+          <Link href='mailto:hello@growupstudio.dk'><a className={css.link}>Skriv os en mail</a></Link>
           </span>
         </div>
 
@@ -50,6 +52,86 @@ export default function ContactSection() {
           <p className='smallPrint contactInner'>All-in-one webshops og hjemmesider, med skræddersyet indhold og billeder, samt organisk indhold til jeres SoMe profiler.</p>
           </div>
         </div>
+
+        <div className={css.contactLinks}>
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>Art Direction</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>E-commerce</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>SEO</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>SoMe</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>UX / UI design</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>Visuelle identitet</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+
+          <div className='contactWrapper'>
+            <span className='contactInner'>
+              <Link href='/'>
+                <a className={css.contactLink}>
+                <span>Webdesign</span>
+                <Image src={arrow} className={css.arrowUpRight}/>
+                </a>
+              </Link>
+            </span>
+          </div>
+        </div>
+
       </div>
 
       </section>
